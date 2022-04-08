c689064afed651a9f0f04135b65284436cb26ac8
FaBbiye <frnz.bagheri@gmail.com>
2022-04-08 10:44:03 +0100

    sendValue change

--- contracts/DemetraPresale.sol
+++ contracts/DemetraPresale.sol
@@ -159,35 +159,7 @@ contract DemetraPresale is Ownable {
         return false;
     }
 
-    /**
-     * @dev Replacement for Solidity's `transfer`: sends `amount` wei to
-     * `recipient`, forwarding all available gas and reverting on errors.
-     *
-     * https://eips.ethereum.org/EIPS/eip-1884[EIP1884] increases the gas cost
-     * of certain opcodes, possibly making contracts go over the 2300 gas limit
-     * imposed by `transfer`, making them unable to receive funds via
-     * `transfer`. {sendValue} removes this limitation.
-     *
-     * https://diligence.consensys.net/posts/2019/09/stop-using-soliditys-transfer-now/[Learn more].
-     *
-     * IMPORTANT: because control is transferred to `recipient`, care must be
-     * taken to not create reentrancy vulnerabilities. Consider using
-     * {ReentrancyGuard} or the
-     * https://solidity.readthedocs.io/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
-     */
-    function sendValue(address payable recipient, uint256 amount) internal {
-        require(
-            address(this).balance >= amount,
-            "Address: insufficient balance"
-        );
-
-        // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
-        (bool success, ) = recipient.call{value: amount}("");
-        require(
-            success,
-            "Address: unable to send value, recipient may have reverted"
-        );
-    }
+    
 
     // send bnb to the contract address
     receive() external payable {
@@ -225,7 +197,9 @@ contract DemetraPresale is Ownable {
             totalBnbReceivedInAllTier += msg.value;
             totalBnbInTierZero += msg.value;
             
-             payable(projectOwner).transfer(address(this).balance);
+             //payable(projectOwner).transfer(address(this).balance);
+             Address.sendValue(payable(projectOwner), address(this).balance);
+             //payable(projectOwner).sendValue(projectOwner,address(this).balance);
         } else if (getWhitelistOne(msg.sender)) {
             require(
                 buyInTierOne[msg.sender] + msg.value >= minAllocaPerUserTierOne,
@@ -248,7 +222,8 @@ contract DemetraPresale is Ownable {
             totalBnbReceivedInAllTier += msg.value;
             totalBnbInTierOne += msg.value;
             //sendValue(projectOwner, address(this).balance);
-            payable(projectOwner).transfer(address(this).balance);
+            //payable(projectOwner).transfer(address(this).balance);
+            Address.sendValue(payable(projectOwner), address(this).balance);
         } else if (getWhitelistTwo(msg.sender)) {
             require(
                 buyInTierTwo[msg.sender] + msg.value >=
@@ -271,8 +246,8 @@ contract DemetraPresale is Ownable {
             buyInTierTwo[msg.sender] += msg.value;
             totalBnbReceivedInAllTier += msg.value;
             totalBnbInTierTwo += msg.value;
-            payable(projectOwner).transfer(address(this).balance);
-
+            //payable(projectOwner).transfer(address(this).balance);
+            Address.sendValue(payable(projectOwner), address(this).balance);
         } else {
             revert();
         }
